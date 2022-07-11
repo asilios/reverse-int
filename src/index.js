146d29fbcb64a3295915a8e7b6f7ce2c9a57d98a
asilios <85323192+asilios@users.noreply.github.com>
2022-07-11 16:50:00 +0500

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {
+        var number = n * -1;
+        return (parseFloat(number.toString().split('').reverse().join('')));
+    } else {
+        return (parseFloat(n.toString().split('').reverse().join('')));
+    }
 }
